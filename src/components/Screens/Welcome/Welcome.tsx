49c23bcf7623f90398dacb32d93ad8b5d1e77bf2
uvanov <kirill.ivanov000@gmail.com>
2022-02-01 17:24:39 +0200

    Basic components

---
+++ src/components/Screens/Welcome/Welcome.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+
+const Welcome = () => {
+    return (
+        <Container>
+            Some text
+        </Container>
+    );
+};
+
+export default Welcome;
