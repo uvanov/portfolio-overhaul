473f6837ca97871446f9c5e545853d75b8141e2f
uvanov <kirill.ivanov000@gmail.com>
2022-02-03 17:35:53 +0200

    Change App styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import Welcome from "./components/Screens/Welcome/Welcome";
+import styled from 'styled-components';
+
+const AppWrapper = styled.div`
+    padding-top: 30px;
+`
 
 const App = () => {
     return (
-        <>
+        <AppWrapper>
             <Welcome/>
-        </>
+        </AppWrapper>
     );
 };
 
