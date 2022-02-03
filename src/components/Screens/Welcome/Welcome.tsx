362d771b93d1faf74c484a0fbe449a71734b701a
uvanov <kirill.ivanov000@gmail.com>
2022-02-03 17:38:04 +0200

    Welcome section layout

--- src/components/Screens/Welcome/Welcome.tsx
+++ src/components/Screens/Welcome/Welcome.tsx
@@ -4,14 +4,45 @@ import Header from "../../Layout/Header/Header";
 import styled from "styled-components";
 
 
-// Styles
+// Assets Import
+
+import WelcomeVKImage from '../../../assets/images/vk.svg';
+import WelcomeTelegramImage from '../../../assets/images/telegram.svg';
+import WelcomeGithubImage from '../../../assets/images/github.svg';
+import WelcomeImage from '../../../assets/images/main-image.jpg';
+import GlowImage from '../../../assets/images/glow.webp';
+
+// Styles Definition
 
 const AboutWrapper = styled.div`
     display: flex;
+    justify-content:center;
     gap: 37px;
+    
+    position:relative;
+    
+    margin-top: 190px;
+    
+    @media screen and (max-width: 1080px){
+      flex-direction: column;
+      align-items:center;
+    }
+    
+    @media screen and (max-width: 800px){
+      align-items:unset;
+    }
 `
 const ImageWrapper = styled.div`
-
+    border-radius: 11px;
+    overflow: hidden;
+    
+    @media screen and (max-width: 450px){
+       display: none;
+    }
+    
+    img{
+      width: 100%;
+    }
 `
 const AboutInformationWrapper = styled.div`
     display:flex;
@@ -30,6 +61,11 @@ const NameText = styled.span`
     font-size: 43px;
     color: #CCD6F6;
     margin: 32px 0 9px;
+    
+    @media screen and (max-width: 450px){
+       font-size: 30px;
+       margin-bottom: 50px;
+    }
 `
 const AboutText = styled.p`
         font-weight: 600;
@@ -37,64 +73,119 @@ const AboutText = styled.p`
         line-height: 21px;     
         color: #8892B0;
         max-width: 430px;
+        
+        @media screen and (max-width: 450px){
+           font-size: 21px;
+        }
 `
 const AboutContactList = styled.ul`
     display: flex;
     gap: 20px;
+    margin-top: 105px;
+    
+    @media screen and (max-width: 1080px){
+       justify-content:center;
+    }
+    
+    @media screen and (max-width: 450px){
+       font-size: 30px;
+       margin-bottom: 50px;
+    }
+    
 `
 const AboutContactLink = styled.a`
     display: flex;
     align-items: center;
     justify-content: center;
-    padding: 17px;
     
+    padding: 17px;
     border: 3px solid #64FFDA;
+    box-sizing: border-box;
+    
+    &:hover {
+       transform: scale(1.1);
+    }
+    
+    @media screen and (max-width: 325px){
+       padding: 10px;
+    }
 `
-
-// Assets
-
+const Glow = styled.img`
+    position:absolute;
+    top: -1100px;
+    left: -1000px;
+    opacity: .8;
+    z-index: -2;
+`
 
 
 const Welcome = () => {
+
+    // Constants
+
+    const CONTACT_LIST = [
+        {
+            url: 'https://vk.com/uvanov_vk',
+            imageUrl: WelcomeVKImage
+        },
+        {
+            url: 'https://t.me/uvanov_tg',
+            imageUrl: WelcomeTelegramImage
+        },
+        {
+            url: 'https://github.com/uvanov',
+            imageUrl: WelcomeGithubImage
+        }
+    ]
+
     return (
-        <Container>
-            <Header
-                button={{innerText: 'Резюме', url: '/'}}
-                links={
-                    [
-                        {
-                            innerText: 'Обо мне',
-                            blockToScroll: '.some-block'
-                        },
-                        {
-                            innerText: 'Работы',
-                            blockToScroll: '.some-block-2'
-                        }
-                    ]
-                }
-            />
+        <>
+            <Container>
+                <Header
+                    button={{innerText: 'Резюме', url: '/'}}
+                    links={
+                        [
+                            {
+                                innerText: 'Обо мне',
+                                blockToScroll: '.some-block'
+                            },
+                            {
+                                innerText: 'Работы',
+                                blockToScroll: '.some-block-2'
+                            }
+                        ]
+                    }
+                />
 
-            <AboutWrapper>
-                <ImageWrapper>
-                    {/*  Image Here  */}
-                </ImageWrapper>
+                <AboutWrapper>
+                    <ImageWrapper>
+                        <img src={WelcomeImage} alt="uvanov"/>
+                    </ImageWrapper>
 
-                <AboutInformationWrapper>
-                    <HelloText>Привет, меня зовут</HelloText>
-                    <NameText>Кирилл Иванов</NameText>
-                    <AboutText>
-                        Я — веб-разработчик, специализирующийся на создании прогрессивных и
-                        удобных сайтов и веб-приложений.
-                    </AboutText>
+                    <AboutInformationWrapper>
+                        <HelloText>Привет, меня зовут</HelloText>
+                        <NameText>Кирилл Иванов</NameText>
+                        <AboutText>
+                            Я — веб-разработчик, специализирующийся на создании прогрессивных и
+                            удобных сайтов и веб-приложений.
+                        </AboutText>
 
-                    <AboutContactList>
-                        <AboutContactLink>
+                        <AboutContactList>
+                            {
+                                CONTACT_LIST.map((ContactLink) => (
+                                    <AboutContactLink href={ContactLink.url}>
+                                        <img src={ContactLink.imageUrl} alt=""/>
+                                    </AboutContactLink>
+                                ))
+                            }
+                        </AboutContactList>
+                    </AboutInformationWrapper>
+                </AboutWrapper>
 
-                        </AboutContactLink>
-                    </AboutContactList>
-                </AboutInformationWrapper>
-            </AboutWrapper>
-        </Container>
+                <Glow src={GlowImage}/>
+            </Container>
+
+        </>
 
     );
 };
